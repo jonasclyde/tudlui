18afbb360996a2a20db6c0490e421c309bf47dae
Jonas Clyde Almocera <jonas@reminisense.com>
2016-11-08 21:46:38 +0800

    Added quiz.

---
+++ public/js/phaser/tudlui/quiz.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Jonas on 11/2/2016.
+ */
+
+var quiz = function(game){
+};
+
+
+quiz.prototype = {
+    init: function(code, name) {
+
+    },
+    preload: function(){
+
+    },
+    create: function(){
+
+        game.background = this.game.add.sprite(0,0, 'background');
+    },
+    update: function(){
+
+    },
+    render: function(){
+
+    }
+
+}
